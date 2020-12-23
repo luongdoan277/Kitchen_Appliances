d6e279b136fc65f7d2abadf4d94c4bf7327b9f25
Khanh Vuong <khanhvuong987@gmail.com>
2020-12-23 15:09:00 +0700

    layout admin,product list

---
+++ Pages/Admin.cshtml.cs
@@ -0,0 +1,12 @@
+﻿using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+
+namespace Kitchen_Appliances.Pages
+{
+    public class Admin : PageModel
+    {
+
+    }
+}
---
+++ wwwroot/js/vertical-menu.js
@@ -0,0 +1,12 @@
+﻿const itemClick = document.querySelectorAll(".item");
+const childLayout = document.querySelectorAll(".child-layout");
+
+console.log(childLayout);
+console.log(itemClick);
+
+itemClick.forEach((item, index) => {
+    item.addEventListener('click', function () {
+        childLayout[index].classList.toggle('active-menu');
+        item.classList.toggle('active-a')
+    });
+});
