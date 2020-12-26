29b98e4b34e9e504dbcc80a464c12c3e5703ebd3
luongdoan277 <luongdoan277@gmail.com>
2020-12-26 17:16:13 +0700

    a

---
+++ Models/ViewModels/AdminListViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Kitchen_Appliances.Models.ViewModels
+{
+    public class AdminListViewModel
+    {
+        public IEnumerable<Product> Products { get; set; }
+        public PagingInfo PagingInfo { get; set; }
+    }
+}
